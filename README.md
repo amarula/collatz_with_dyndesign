97ddb5a12c03d52834757eda3587826d5e41afa4
Patrizio Gelosi <patrizio.gelosi@amarulasolutions.com>
2023-05-05 15:44:05 +0200

    Examples from article on merging classes
    Code examples from article on merging classes are added.
    
    Signed-off-by: Patrizio Gelosi <patrizio.gelosi@amarulasolutions.com>

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# collatz_with_dyndesign
-Implementing collatz using dyndesign merging classes
+# Collatz With DynDesign
+
+Use cases of [DynDesgin](https://github.com/amarula/dyndesign) utilized to develop a script for exploring Collatz functions, published at *&lt;link&gt;*.
---
+++ examples/collatz.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import argparse
+
+class Collatz:
+    FUNCTION_NAME = 'Collatz'
+
+    def __init__(self, n):
+        self.n = int(n)
+
+    def get_next_n(self, n):
+        if n % 2 == 0:
+            return n // 2
+        else:
+            return n*3 +1
+
+    def get_collatz_sequence(self, n):
+        while n != 1:
+            n = self.get_next_n(n)
+            yield n
+
+    def output_number(self, n):
+        print(f"{n}; ", end='')
+
+    def output_sequence(self):
+        print(f"{self.FUNCTION_NAME} sequence starting from {self.n} is:")
+        self.output_number(self.n)
+        for n in self.get_collatz_sequence(self.n):
+            self.output_number(n)
+        print()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Calculate Collatz sequence.')
+    parser.add_argument('n')
+    args = parser.parse_args()
+    collatz = Collatz(args.n)
+    collatz.output_sequence()
